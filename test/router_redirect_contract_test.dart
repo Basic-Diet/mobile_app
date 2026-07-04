0edcbd88cd8c7cabaa5ba049086751cd84954117
moalimoali009-bit <moalimoali009@gmail.com>
2026-07-04 03:17:33 +0300

    Avoid current user call in router redirect

---
+++ test/router_redirect_contract_test.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('global router redirect does not call the current-user backend', () {
+    final routesManager = File(
+      'lib/presentation/resources/routes_manager.dart',
+    ).readAsStringSync();
+
+    expect(routesManager, isNot(contains('GetCurrentUserUseCase')));
+    expect(routesManager, isNot(contains('getCurrentUser')));
+    expect(routesManager, isNot(contains('redirect:')));
+  });
+}
